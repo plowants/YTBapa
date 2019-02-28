c71a39f5f14150afaa1784b198aa858cc00a1da0
Liu.M <lxf276541049@163.com>
2019-02-28 09:51:08 +0800

    升级nuget包与升级.net core版本到2.2

--- src/Tools/YTBapa.PanelTool/Program.cs
+++ src/Tools/YTBapa.PanelTool/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using ServiceStack;
+using ServiceStack.Text;
+using RabbitMQ.Client;
 
-namespace YTbapa.PanelTool
+namespace YTBapa.PanelTool
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DateTime time = DateTime.Now;
+            var  time2 = SystemTime.UtcNow;
+
+            Console.WriteLine(TimeSpan.MinValue);
+            Console.WriteLine(time);
+            Console.WriteLine(time2);
+            var t = time.ToUniversalTime() - (new DateTime(1970, 1, 1));
+            long longT = (long)t.TotalMilliseconds;
+            Console.WriteLine(longT);
+            var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            Console.WriteLine(startTime.AddMilliseconds(longT));
+
+            TestInput input = new TestInput()
+            {
+                Time = longT,
+            };
+            var str = input.ToJson<TestInput>();
+            Console.WriteLine(str);
+            var value = JsonSerializer.DeserializeFromString<DateTime>(longT.ToString());
+            Console.WriteLine(value);
+            TestDto dto = str.FromJson<TestDto>();
+            Console.WriteLine(dto.Time);
             Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+
+        static DateTime ConvertTimestamp(double timestamp)
+        {
+            DateTime converted = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            DateTime newDateTime = converted.AddSeconds(timestamp);
+            return newDateTime.ToLocalTime();
         }
     }
 }
---
+++ src/Tools/YTBapa.PanelTool/TestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YTBapa.PanelTool
+{
+    public class TestDto
+    {
+        public DateTime Time { get; set; }
+
+    }
+
+    public class TestInput
+    {
+        public double Time { get; set; }
+    }
+}
